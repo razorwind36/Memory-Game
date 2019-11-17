d127f006a60c5510fd3da1e14fa451ee3a9a152b
Zihni Can Kalsin <cankalsin@Zihnis-MacBook-Pro.local>
2019-11-17 05:56:50 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,3 +1,4 @@
+/* 
 console.log("Up and running!");
 
 var cardOne = "queen";
@@ -6,3 +7,21 @@ var cardThree = "king";
 var cardFour = "king";
 
 console.log("User flipped " + cardOne);
+*/
+
+var cards = ["queen","queen","king","king"];
+
+var cardsInPLay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPLay.push(cardOne);
+cardsInPLay.push(cardTwo);
+
+console.log("User has flipped " + cardOne);
+console.log("User has flipped " + cardTwo);
+
+if(cardsInPLay.length === 2){
+    if(cardsInPLay[0] === cardsInPLay[1]) alert("You found a match!");
+    else alert("Sorry, try again");
+};
